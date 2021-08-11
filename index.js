ecb891e268229b1d7b284be84c6da9fa55c216f0
dcaldwell1228 <dcaldwell1228@gmail.com>
2021-08-11 13:29:33 -0400

    add to repo

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    setInterval(
+        ()=>{
+            var blur = document.getElementById("blur").value;
+            var brightness = document.getElementById("brightness").value;
+            var contrast = document.getElementById("contrast").value;
+            var grayscale = document.getElementById("grayscale").value;
+            var hue = document.getElementById("hue").value;
+            var invert = document.getElementById("invert").value;
+            var opacity = document.getElementById("opacity").value;
+            var saturate = document.getElementById("saturate").value;
+            var sepia = document.getElementById("sepia").value;
+            var image = document.getElementById("image");
+
+            image.style.filter = "blur("+ blur +"px) "
+            + "brightness("+ brightness +"%) "
+            + "contrast("+ contrast +"%) "
+            + "grayscale("+ grayscale +"%) "
+            + "hue-rotate("+ hue +"deg) "
+            + "invert("+ invert +"%) "
+            + "opacity("+ opacity +"%) "
+            + "saturate("+ saturate +"%) "
+            + "sepia("+ sepia +"%) ";
+        });
+});
